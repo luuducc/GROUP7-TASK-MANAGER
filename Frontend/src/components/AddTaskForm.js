f123db63b0aa363b239b0dac28663e3fa4f8923c
Nguyễn Văn Đức <nguyenvanduc@MacBook-Air-cua-Nguyen-6.local>
2023-12-04 14:52:43 +0700

    CRUD Task

---
+++ Frontend/src/components/AddTaskForm.js
@@ -0,0 +1,39 @@
+// AddTaskForm.js
+import React from 'react';
+
+const AddTaskForm = ({ newTask, setNewTask, handleAddTask, onClose }) => {
+  return (
+    <div className="popup">
+      <div className="popup-content">
+        <h3>Thêm Task</h3>
+        <label>
+          Title:
+          <input
+            type="text"
+            value={newTask.title}
+            onChange={(e) => setNewTask({ ...newTask, title: e.target.value })}
+          />
+        </label>
+        <label>
+          Body:
+          <textarea
+            value={newTask.body}
+            onChange={(e) => setNewTask({ ...newTask, body: e.target.value })}
+          />
+        </label>
+        <label>
+          Image URL:
+          <input
+            type="text"
+            value={newTask.image}
+            onChange={(e) => setNewTask({ ...newTask, image: e.target.value })}
+          />
+        </label>
+        <button onClick={handleAddTask}>Thêm</button>
+        <button onClick={onClose}>Đóng</button>
+      </div>
+    </div>
+  );
+};
+
+export default AddTaskForm;
