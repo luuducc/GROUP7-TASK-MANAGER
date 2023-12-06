50337a69c8dcef71eafc92eee2311909f427ed28
luuduc <luuduc1708@gmail.com>
2023-12-06 17:36:32 +0700

    create seeHandler module

--- Backend/app.js
+++ Backend/app.js
@@ -4,9 +4,7 @@ const mongoose = require("mongoose");
 const taskRouter = require("./routes/TaskRoutes");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
-const cron = require("node-cron")
-const { addClient, removeClient, sendSSEMessage } = require("./utils")
-const Task = require('./models/Task');
+const { setupSSEEndpoint, setupCronJob } = require('./sseHandler')
 const cors = require("cors");  // Import the cors middleware
 require('dotenv').config()
 
@@ -28,73 +26,10 @@ const start = async() => {
     console.log('connected to the db')
 
     // Set up SSE Endpoint
-    app.get('/events', (req, res) => {
-      res.setHeader('Content-Type', 'text/event-stream')
-      res.setHeader('Cache-Control', 'no-cache')
-      res.setHeader('Connection', 'keep-alive')
-
-      const clientId = Date.now()
-      res.flushHeaders()
-
-      addClient( {id: clientId, res})
-
-      req.on("close", () => {
-        removeClient(clientId)
-      })
-    })
+    setupSSEEndpoint(app)
 
     // Start cron job
-    cron.schedule("*/5 * * * *", async () => { // run every 5 minutes
-      try {
-        const tasks = await Task.find()
-        const today = new Date();
-        tasks.forEach(task => {
-          const { expiredDate, customNoti } = task;
-          const { value: timeUntilExpiration } = customNoti
-          const timeDiff = expiredDate.getTime() - today.getTime();
-          const minutesUntilExpiration = timeDiff / (1000 * 60)
-          const hoursUntilExpiration = minutesUntilExpiration / 60
-          const daysUntilExpiration = Math.ceil(timeDiff / (1000*3600*24));
-
-          console.log({ daysUntilExpiration, customNoti, timeUntilExpiration })
-
-          switch(customNoti.time) {
-            case "day":   
-              if(daysUntilExpiration <= timeUntilExpiration) {
-                sendSSEMessage({
-                  name: task.title,
-                  daysUntilExpiration,
-                  message: `the time left is ${daysUntilExpiration} day`
-                })
-              }
-              break
-            case "hour" :
-              if(hoursUntilExpiration <= timeUntilExpiration) {
-                sendSSEMessage({
-                  name: task.title,
-                  hoursUntilExpiration,
-                  message: `the time left is ${hoursUntilExpiration} hours`
-                })
-              }
-              break
-            case "minute" :
-              if(hoursUntilExpiration <= timeUntilExpiration) {
-                sendSSEMessage({
-                  name: task.title,
-                  minutesUntilExpiration,
-                  message: `the time left is ${minutesUntilExpiration} minute`
-                })
-              }
-              break
-            default: 
-              break
-          }
-          
-        })   
-      } catch (error) {
-        console.error("Error: " + error.message)
-      }
-    })
+    setupCronJob()
     //
     app.listen(3000, () => {
       console.log("Server is running on http://localhost:3000/");
