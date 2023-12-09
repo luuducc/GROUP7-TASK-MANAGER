e582555dd5ebd532ebf0c65d91327cdafdbf0bd2
luuduc <luuduc1708@gmail.com>
2023-12-10 00:39:31 +0700

    create temporary API for db-reference testing

--- Backend/routes/TaskRoutes.js
+++ Backend/routes/TaskRoutes.js
@@ -7,6 +7,7 @@ const {
   deleteTask,
   getTaskByTitle, 
   deleteAllTask,
+  getTaskByUser
 } = require("../controllers/TaskController");
 
 const router = express.Router();
@@ -14,5 +15,6 @@ const router = express.Router();
 router.route("/").get(getAllTasks).post(createTask).delete(deleteAllTask); 
 router.route("/title/:title").get(getTaskByTitle)
 router.route("/:id").get(getTaskById).put(updateTask).delete(deleteTask);
+router.route("/user/:user").get(getTaskByUser); // temporary API
 
 module.exports = router;
