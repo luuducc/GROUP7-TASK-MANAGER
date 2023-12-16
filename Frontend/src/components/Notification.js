675f77f8afd3acf57736349a79ac4e8d1131b2ba
Nguyễn Văn Đức <nguyenvanduc@MacBook-Air-cua-Nguyen-6.local>
2023-12-16 17:11:00 +0700

    Notification UI

--- Frontend/src/components/Notification.js
+++ Frontend/src/components/Notification.js
@@ -1,12 +1,37 @@
-
 import React, { useState, useEffect } from 'react';
 import './Notification.css';
 
 const Notification = () => {
+  const [notifications, setNotifications] = useState([]);
+
+  const addNotification = (message) => {
+    const newNotification = {
+      id: Date.now(),
+      message,
+    };
+
+    setNotifications(prevNotifications => [...prevNotifications, newNotification]);
+  };
+
+  useEffect(() => {
+    addNotification('This is a permanent notification');
+  }, []);
+
+  const removeNotification = (id) => {
+    setNotifications(prevNotifications => prevNotifications.filter(notification => notification.id !== id));
+  };
 
   return (
     <div className="Notification-container">
-        <h2>Notification</h2>
+      <h2>Notification</h2>
+      <ul className="notification-list">
+        {notifications.map(notification => (
+          <li key={notification.id} className="notification-item">
+            {notification.message}
+            <button onClick={() => removeNotification(notification.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
