4be783ac21f44dc6471c0c8c6058b92f5469d9d1
nvduc <nvduc>
2023-12-19 01:42:34 +0700

    done profile

--- Frontend/src/components/DeleteAccount.js
+++ Frontend/src/components/DeleteAccount.js
@@ -2,54 +2,97 @@ import 'react-toastify/dist/ReactToastify.css';
 import { toast } from 'react-toastify';
 import { Button, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton } from '@chakra-ui/react';
 import { Box } from '@chakra-ui/react';
-import {Flex } from '@chakra-ui/react';
-import './Profile.css';
-import React, { useState } from 'react';
+import { Flex } from '@chakra-ui/react';
+import { Input, InputGroup, InputRightElement } from '@chakra-ui/react';
+import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const DeleteAccount = ({ onClose, displayToast }) => {
+  const userId = localStorage.getItem('userId');
+  const token = localStorage.getItem('token');
+  const navigate = useNavigate();
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
-    const userId = localStorage.getItem('userId');
-    const token = localStorage.getItem('token');
-    const navigate = useNavigate();
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const handleConfirmDelete = async () => {
-        try {
-          await axios.delete(`http://localhost:3000/user/delete/${userId}`, {
-            headers: {
-              token: `Bearer ${token}`,
-            },
-          });
-    
-          localStorage.removeItem('userId');
-          localStorage.removeItem('token');
-          navigate('/');
-        } catch (error) {
-          console.error('Error deleting user account:', error);
-        } finally {
-            onClose();
-        }
-      };
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
-    return (
-        <div >
-            <ModalOverlay />
-                <ModalContent>
-                <ModalHeader textAlign="center">Edit Information</ModalHeader>
-                    <ModalCloseButton />
-                    <Box p={5} textAlign="center">
-                      <p style={{ marginBottom: '20px' }}>Are you sure you want to delete your account?</p>
-                      <Flex justifyContent="space-around">
-                        <Button colorScheme='red' onClick={handleConfirmDelete} marginRight="10px">
-                          Yes, Delete Account
-                        </Button>
-                        <Button colorScheme='blue' onClick={onClose}>Cancel</Button>
-                      </Flex>
-                    </Box>
-                </ModalContent>
-        </div>
-    )
-}
+  const handleConfirmDelete = async () => {
+    try {
+      // check password
+      const response = await axios.post(`http://localhost:3000/user/checkPass/${userId}`, {
+          password,
+      }, {
+          headers: {
+              token: `Bearer ${token}`
+          }
+      });
+
+      console.log(">>> check response: ", response.data);
+
+      if (response.data === 'Done') {
+        // delete account
+        await axios.delete(`http://localhost:3000/user/delete/${userId}`, {
+          headers: {
+            token: `Bearer ${token}`,
+          },
+        });
+
+        localStorage.removeItem('userId');
+        localStorage.removeItem('token');
+        navigate('/');
+      } else {
+        toast.error('Incorrect password. Please try again.');
+      }
+    } catch (error) {
+      toast.error('Incorrect password. Please try again.');
+      console.error('Error deleting user account:', error);
+    } finally {
+      onClose();
+    }
+  };
+
+  return (
+    <div>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader textAlign="center">Delete Account</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Box p={5} textAlign="center">
+            <p style={{ marginBottom: '20px' }}>Enter your password to delete your account:</p>
+            <InputGroup size="md">
+              <Input
+                type={showPassword ? 'text' : 'password'}
+                placeholder="Password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+              <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={handleTogglePasswordVisibility}>
+                  {showPassword ? 'Hide' : 'Show'}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            <Flex justifyContent="space-around" mt={4}>
+              <Button colorScheme="red" onClick={handleConfirmDelete} marginRight="10px">
+                Yes, Delete Account
+              </Button>
+              <Button colorScheme="blue" onClick={onClose}>
+                Cancel
+              </Button>
+            </Flex>
+          </Box>
+        </ModalBody>
+      </ModalContent>
+    </div>
+  );
+};
 
 export default DeleteAccount;
